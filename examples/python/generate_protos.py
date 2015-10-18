6fe614b8d18d1c2be1c99f2d79f61785dfa162fd
user_yangjun <364223@qq.com>
2015-10-19 01:24:23 +0800

    use six use my_bin_dir to reflect new biicode style layout

--- examples/python/generate_protos.py
+++ examples/python/generate_protos.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+import six
 
 from rpcz import compiler
+import os
+
+my_bin_dir=os.path.abspath('../../../../../../bin/vc90/x86/debug')
+os.environ['PATH']+=';'+my_bin_dir
 
 compiler.generate_proto('../common/search.proto', '.')
 compiler.generate_proto(
         '../common/search.proto', '.',
         with_plugin='python_rpcz', suffix='_rpcz.py',
         plugin_binary=
-            '../../build/src/rpcz/plugin/python/protoc-gen-python_rpcz')
+            my_bin_dir+r'\protoc-gen-python_rpcz')
