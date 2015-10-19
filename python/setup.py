7d093fa593ad7a78927c7249497899126b55b88a
user_yangjun <364223@qq.com>
2015-10-19 16:08:46 +0800

    import future and fix error reformat

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,11 @@
+# coding=utf-8
+# python 2.7
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+import six
+
 import os
 import compiler
 import shutil
@@ -6,7 +14,8 @@ from distutils.command import build as build_module
 from distutils.extension import Extension
 from distutils.core import setup
 
-BUILD_DIR=os.environ['build_dir']
+BUILD_DIR = os.environ['build_dir']
+
 
 def _build_rpcz_proto():
     compiler.generate_proto('../src/rpcz/proto/rpcz.proto', 'rpcz')
@@ -15,10 +24,10 @@ def _build_rpcz_proto():
 def _build_test_protos():
     compiler.generate_proto('../test/proto/search.proto', 'tests')
     compiler.generate_proto(
-            '../test/proto/search.proto', 'tests',
-            with_plugin='python_rpcz', suffix='_rpcz.py',
-            plugin_binary=
-                BUILD_DIR + '/src/rpcz/plugin/python/protoc-gen-python_rpcz')
+        '../test/proto/search.proto', 'tests',
+        with_plugin='python_rpcz', suffix='_rpcz.py',
+        plugin_binary=
+        BUILD_DIR + '/src/rpcz/plugin/python/protoc-gen-python_rpcz')
 
 
 class build(build_module.build):
@@ -31,26 +40,30 @@ class build(build_module.build):
 
 class gen_pyext(Command):
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         if os.system('cython --cplus cython/pywraprpcz.pyx') != 0:
             raise IOError("Running cython failed.")
 
-all_library_dirs=[ BUILD_DIR + '/deps/lib', BUILD_DIR + '/src/rpcz']
+
+all_library_dirs = [BUILD_DIR + '/deps/lib', BUILD_DIR + '/src/rpcz']
 all_library_dirs.extend(os.environ['link_dirs'].split(';'))
 
 setup(
-    name = "rpcz",
-    version = "0.9",
-    author = "Nadav Samet",
-    author_email = "nadavs@google.com",
-    description = "An RPC implementation for Protocol Buffer based on ZeroMQ",
-    license = "Apache 2.0",
-    keywords = "protocol-buffers rpc zeromq 0mq",
-    packages=['rpcz', 'tests'],
+    name="rpcz",
+    version="0.9",
+    author="Nadav Samet",
+    author_email="nadavs@google.com",
+    description="An RPC implementation for Protocol Buffer based on ZeroMQ",
+    license="Apache 2.0",
+    keywords="protocol-buffers rpc zeromq 0mq",
+    packages=[b'rpcz', b'tests'],
     url='http://code.google.com/p/rpcz/',
     long_description='',
     classifiers=[
@@ -58,22 +71,20 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License",
     ],
-    cmdclass = {
+    cmdclass={
         'build': build,
         'gen_pyext': gen_pyext,
     },
     ext_modules=[
-        Extension("rpcz.pywraprpcz", ["cython/pywraprpcz.cpp"],
-                  libraries=["rpcz","libprotobuf","libzmq-static","python27","ws2_32","rpcrt4","iphlpapi","advapi32"],
-                  include_dirs=['../include', BUILD_DIR + '/src',os.environ['BOOST_ROOT'],os.environ['PROTOBUF_SRC_ROOT_FOLDER'],os.environ['ZEROMQ_ROOT']],
+        Extension(b"rpcz.pywraprpcz", [b"cython/pywraprpcz.cpp"],
+                  libraries=["rpcz", "libprotobuf", "libzmq-static", "python27", "ws2_32", "rpcrt4", "iphlpapi", "advapi32"],
+                  include_dirs=['../include', BUILD_DIR + '/src', os.environ['BOOST_ROOT'], os.environ['PROTOBUF_SRC_ROOT_FOLDER'], os.environ['ZEROMQ_ROOT']],
                   library_dirs=all_library_dirs,
                   language='c++',
-				  extra_compile_args=['/MT'],
-				  #extra_compile_args=['/MTd','/DPy_NO_ENABLE_SHARED','/D_DEBUG'],
-				  #undef_macros=['NDEBUG']
-				  #define_macros=['Py_NO_ENABLE_SHARED','_DEBUG']
-				  )
+                  extra_compile_args=['/MT'],
+                  # extra_compile_args=['/MTd','/DPy_NO_ENABLE_SHARED','/D_DEBUG'],
+                  # undef_macros=['NDEBUG']
+                  # define_macros=['Py_NO_ENABLE_SHARED','_DEBUG']
+                  )
     ],
 )
-
-
