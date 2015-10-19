e27e775ec69f4ba716f31151a5968d3f63b61407
user_yangjun <364223@qq.com>
2015-10-19 16:06:57 +0800

    prepend utf8 declaration ahead fix indent query for asian language

--- examples/python/example_client.py
+++ examples/python/example_client.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+# coding=utf-8
+# python 2.7
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 import six
+
 import rpcz
 import search_pb2
 import search_rpcz
@@ -11,8 +13,10 @@ import search_rpcz
 app = rpcz.Application()
 
 stub = search_rpcz.SearchService_Stub(
-        app.create_rpc_channel("tcp://127.0.0.1:5555"))
+    app.create_rpc_channel("tcp://127.0.0.1:5555"))
 
 request = search_pb2.SearchRequest()
-request.query = 'gold'
-print(stub.Search(request, deadline_ms=1000))
+request.query = '元宝'
+response = stub.Search(request, deadline_ms=1000)
+print(response)
+print(response.results)
